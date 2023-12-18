68c06e79ef4412db813d81e9c12aed217154c49d
r0b3rtb03 <39073631+r0b3rtb03@users.noreply.github.com>
2023-12-18 10:48:45 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,83 +1,95 @@
+def calculate_distance():
 
-# Inputs
-#steps = int(input("How many steps have you've taken?: "))
-#walking_speed = float(input("How fast were you walking?: "))
-#gender = input("What gender are you?: ")
-miles = 5280 # in feet
-while True: # Steps
-    try:
-        steps = int(input("How many steps have you've taken?: "))
-        if steps >= 0:
-            break
-        else:
-            print("Invalid input. Please enter positive a number: ")
-    except ValueError:
-        print("Invalid input. Please enter a number: ")
+    # Def functions
+    def positive_number_int(prompt):
+        while True:
+            try:
+                value = int(input(prompt))
+                if value >= 0:
+                    return value
+                else:
+                    print("Invalid input. Please enter positive a number: ")
+            except ValueError:
+                print("Invalid input. Please enter a number: ")
 
-while True: # Walking Speed
-    try:
-        walking_speed = float(input("How fast were you walking? (mph): "))
-        if walking_speed > 0:
-            if walking_speed > 27.78:
-                print("Invalid input. Usain Bolt could only run 27.78 mph and he holds the record for fastest man alive.")
+    def positive_number_float(prompt, max_value=None):
+        while True:
+            try:
+                value = float(input(prompt))
+                if value > 0 and (max_value is None or value <= max_value):
+                    return value
+                else:
+                    if max_value is not None:
+                        print(f"Invalid input. Please enter a positive number up to {max_value}.")
+                    else:
+                        print("Invalid input. Please enter a positive number.")
+            except ValueError:
+                print("Invalid input. Please enter a number: ")
+
+    def gender_input(prompt):
+        while True:  # Gender
+            gender = input(prompt)
+            if gender.lower() in ["male", "female"]:
+                return gender.lower()
             else:
-                break
-        else:
-            print("Invalid input. Please enter a positive number: ")
-    except ValueError:
-        print("Invalid input. Please enter a number: ")
+                print("Invalid input. Please enter 'Male' or 'Female'.")
 
-while True: # Gender
-    gender = input("What gender are you?: ")
+    def feet_input(prompt):
+        while True:
+            try:
+                value = int(input(prompt))
+                if value >= 0:
+                    return value
+                else:
+                    print("Invalid input. Please enter positive a number: ")
+            except ValueError:
+                print("Invalid input. Please enter a number: ")
 
-    if gender.lower() in ["male", "female"]:
-        break
-    else:
-        print("Invalid input. Please enter 'Male' or 'Female'.")
+    def inch_input(prompt):
+        while True:
+            try:
+                value = int(input(prompt))
+                if value >= 0:
+                    return value
+                else:
+                    print("Invalid input. Please enter a positive number: ")
+            except:
+                print("Invalid input. Please enter a number: ")
 
-# Height, Feet -> Inches -> CM
-print("Input your height")
-while True: # Feet
-    try:
-        h_feet = int(input("Input feet: "))
-        if steps >= 0:
-            break
-        else:
-            print("Invalid input. Please enter positive a number: ")
-    except ValueError:
-        print("Invalid input. Please enter a number: ")
-while True: # Inches
-    try:
-        h_inch = int(input("Input inches: "))
-        if h_inch >= 0:
-            break
-        else:
-            print("Invalid input. Please enter a positive number: ")
-    except:
-        print("Invalid input. Please enter a number: ")
+    # Instance Attributes
 
-# Equations/Formula
+    steps = positive_number_int("How many steps have you taken?: ")
+    walking_speed = positive_number_float("How fast were you walking? (mph): ", max_value=27.78)
+    gender = gender_input("Input your gender (Male/Female): ")
+    print("Input your height")
+    h_feet = feet_input("Input feet: ")
+    h_inch = inch_input("Input inches: ")
+    miles = 5280  # in feet
 
-# Height to CM Formula
-h_inch += (int(h_feet)*12)
-height = round(int(h_inch)*2.54,1)
-print("You are "+ str(height) +" cm's tall") # Test to see if it works
+    # Equations/Formula
 
-# Height to Stride Length Formula
-if gender.lower() == "male":
-    stride_length = h_inch * 1.0541
-elif gender.lower() == "female":
-    stride_length = h_inch * 1.0414
+    # Height to CM Formula
+    h_inch += (int(h_feet) * 12)
+    height = round(int(h_inch) * 2.54, 1)
+    print(f"You are {height} cm's tall")  # Test to see if it works
 
-print("Your length stride is "+str(stride_length)+" cm's") # Test to see if it works
+    # Height to Stride Length Formula
+    if gender.lower() == "male":
+        stride_length = h_inch * 1.0541
+    elif gender.lower() == "female":
+        stride_length = h_inch * 1.0414
 
-# Stride Length to Inches to Feet
-stride_length = round((float((stride_length * 0.39) / 12)),2)
-print("Your length stride is "+str(stride_length)+" ft") # Test to see if it works
+    print(f"Your length stride is {stride_length} cm's")  # Test to see if it works
 
-# Convert Steps to Miles
-avg_mile = round((miles / stride_length), 2)
+    # Stride Length to Inches to Feet
+    stride_length = round((float((stride_length * 0.39) / 12)), 2)
+    print(f"Your length stride is {stride_length} ft")  # Test to see if it works
+
+    # Convert Steps to Miles
+    avg_mile = round((miles / stride_length), 2)
+
+    print(f"You've walked: {steps} steps")
+    print(f"You've walked: {avg_mile} miles")
 
 
-print("You've walked: "+str(steps)+" steps")
-print("You've walked: " +str(avg_mile)+" miles")
+calculate_distance()
